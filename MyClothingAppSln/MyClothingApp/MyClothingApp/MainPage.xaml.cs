aad7c2e46b6f3c1a0235f47506244ecb52be16ab
Mesha <4270714@myuwc.ac.za>
2022-06-04 10:10:14 +0200

    Added code

---
+++ MyClothingAppSln/MyClothingApp/MyClothingApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyClothingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await Task.Delay(3000);
+
+
+
+
+        }
+    }
+
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# ClothingApp
